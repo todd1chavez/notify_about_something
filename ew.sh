043584d36bc47a769208e1e76368828d1d247db6
Harry Potter <harry.potter@gmail.com>
2025-10-16 14:35:52 +0300

    Обновлен запуск скрипта для crontab

--- ew.sh
+++ ew.sh
@@ -1,6 +1,11 @@
-#!/usr/bin/bash
+#!/bin/bash
+
+export DISPLAY=:0
+
+SCRIPT_DIR="$( cd "$( dirname "$0" )" &> /dev/null && pwd )"
 
 FILENAME="main.py"
-PATH_TO_FILE="$PWD/$FILENAME"
 
-python3 $PATH_TO_FILE
+PATH_TO_FILE="$SCRIPT_DIR/$FILENAME"
+
+/usr/bin/env python3 "$PATH_TO_FILE"
